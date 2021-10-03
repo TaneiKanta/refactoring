ca3a4e8a55a6b7e0c9b8165b8643cdb78816f15d
TaneiKanta <hetaleotu@gmail.com>
2021-10-03 22:16:01 +0900

    [chapter_10] 条件記述の統合

--- chapter_10/chapter_10_2.js
+++ chapter_10/chapter_10_2.js
@@ -1,3 +1,21 @@
-if (anEmployee.seniority < 2) return 0;
-if (anEmployee.monthsDisabled > 12) return 0;
-if (anEmployee.isPertTime) return 0;
+if (isNotEligibleForDisability()) return 0;
+
+function isNotEligibleForDisability() {
+    return ((anEmployee.seniority < 2)
+            || (anEmployee.monthsDisabled > 12)
+            || (anEmployee.isPartTime));
+}
+
+
+// 入れ子の場合
+
+if (anEmployee.onVacation)
+  if(anEmployee.seniority > 10)
+    return 1;
+return 0.5
+
+// ↓リファクタリング後
+
+if ((anEnployee.onVacation)
+    && (anEmployee.seniority > 10)) return 1;
+return 0.5
