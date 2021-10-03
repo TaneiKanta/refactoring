20e64fc33422213ef274984d162df0f4cd05e365
TaneiKanta <hetaleotu@gmail.com>
2021-10-03 20:17:52 +0900

    [chapter_10] 条件記述の分解

--- chapter_10/chapter_10_1.js
+++ chapter_10/chapter_10_1.js
@@ -1,5 +1,14 @@
-if (!aDate.isBefore(plan.summmerStart) && !aDate.isAfter(plan.summerEnd))
-  charge = quantity * plan.summerRate;
-else
-  charge = quantity * plan.regularRate + plan.regularServiceCharge;
-end
+charge = summer() ? summerCharge() : regularCharge();
+
+
+function summer() {
+    return !aDate.isBefore(plan.summmerStart) && !aDate.isAfter(plan.summerEnd)
+}
+
+function summerCharge() {
+    return quantity * plan.summerRate;
+}
+
+function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+}
