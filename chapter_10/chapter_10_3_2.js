70245e83704e160c0c8e8357d31c7f2f72273d92
TaneiKanta <hetaleotu@gmail.com>
2021-10-05 02:03:08 +0900

    add

--- chapter_10/chapter_10_3_2.js
+++ chapter_10/chapter_10_3_2.js
@@ -1,20 +1,11 @@
 function payAmount(employ) {
-    let result;
-    if (employee.isSeparated) {
-        result = { amount: 0, reasonCode: "SEP" };
-    }
-    else {
-        if (employee.isRetired) {
-            result = { amount: 0, reasonCode: "RET" };
-        }
-        else {
-            // 金額を計算するロジック
-            lorem.ipsum(dolor.sitAmet);
-            consectetur(adipiscing).elit();
-            sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-            ut.enim.ad(minim.veniam);
-            result = someFinalComputation();
-        }
-    }
-    return result;
+    if (employee.isSeparated) return { amount: 0, reasonCode: "SEP" };
+    if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+
+    // 金額を計算するロジック
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    ut.enim.ad(minim.veniam);
+    return someFinalComputation();
 }
